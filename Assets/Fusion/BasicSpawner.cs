563722375699096d0b12420806d21ac96a00e7c9
IceFong <ice2002.5@gmail.com>
2023-10-19 19:09:24 +0800

    Initial commit

---
+++ Assets/Fusion/BasicSpawner.cs
@@ -0,0 +1,115 @@
+using System;
+using System.Collections.Generic;
+using Fusion;
+using Fusion.Sockets;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BasicSpawner : MonoBehaviour, INetworkRunnerCallbacks
+{
+    //VARIABLES
+    private NetworkRunner _runner;
+    [SerializeField] private NetworkPrefabRef _playerPrefab;
+    private Dictionary<PlayerRef, NetworkObject> _spawnedCharacters = new Dictionary<PlayerRef, NetworkObject>();
+    private bool _mouseButton0;
+    private bool _mouseButton1;
+
+    /////
+
+    public void OnConnectedToServer(NetworkRunner runner) {}
+    public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason) {}
+    public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token) {}
+    public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data) {}
+    public void OnDisconnectedFromServer(NetworkRunner runner) {}
+    public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken) {}
+    public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input) {}
+    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ArraySegment<byte> data) {}
+    public void OnSceneLoadDone(NetworkRunner runner) {}
+    public void OnSceneLoadStart(NetworkRunner runner) {}
+    public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList) {}
+    public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason) {}
+    public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message) {}
+    
+    // Hosting //
+    public void OnPlayerJoined(NetworkRunner runner, PlayerRef player) {
+        if (runner.IsServer) {
+            // Create unique position for player
+            Vector3 spawnPos = new Vector3( (player.RawEncoded % runner.Config.Simulation.DefaultPlayers)*3,1,0);
+            NetworkObject networkPlayerObject = runner.Spawn(_playerPrefab, spawnPos, Quaternion.identity, player);
+            // Keep track of the player avatars so we can remove it when they disconnect
+            _spawnedCharacters.Add(player, networkPlayerObject);
+        }
+    }
+
+    public void OnPlayerLeft(NetworkRunner runner, PlayerRef player) {
+        // Find and remove player avatar
+        if (_spawnedCharacters.TryGetValue(player, out NetworkObject networkObject)) {
+            runner.Despawn(networkObject);
+            _spawnedCharacters.Remove(player);
+        }
+    }
+
+    // Player data transfer //
+    public void OnInput(NetworkRunner runner, NetworkInput input) {
+
+        var data = new NetworkInputData();
+
+        if (Input.GetKey(KeyCode.W)) 
+            data.direction += Vector3.forward;
+        if (Input.GetKey(KeyCode.A)) 
+            data.direction += Vector3.left;
+        if (Input.GetKey(KeyCode.S)) 
+            data.direction += Vector3.back;
+        if (Input.GetKey(KeyCode.D)) 
+            data.direction += Vector3.right;
+
+        if (_mouseButton0)
+            data.buttons |= NetworkInputData.MOUSEBUTTON1;
+        _mouseButton0 = false;
+
+        if (_mouseButton1)
+            data.buttons |= NetworkInputData.MOUSEBUTTON2;
+        _mouseButton1 = false;
+
+        input.Set(data);
+    }
+
+    private void Update() {
+        _mouseButton0 = _mouseButton0 || Input.GetMouseButton(0);
+        _mouseButton1 = _mouseButton1 || Input.GetMouseButton(1);
+    }
+
+    // Start Game Method //
+    async void StartGame(GameMode mode)
+    {
+        // Create the Fusion runner and let it know that we will be providing user input
+        _runner = gameObject.AddComponent<NetworkRunner>();
+        _runner.ProvideInput = true;
+
+        // Start or join (depends on gamemode) a session with a specific name
+        await _runner.StartGame(new StartGameArgs()
+        {
+            GameMode = mode,
+            SessionName = "TestRoom",
+            Scene = SceneManager.GetActiveScene().buildIndex,
+            SceneManager = gameObject.AddComponent<NetworkSceneManagerDefault>()
+        });}
+
+    // client choose to host or join session //
+    private void OnGUI() {
+
+        if (_runner == null) {
+
+            if (GUI.Button(new Rect(0,0,200,40), "Host")) {
+                StartGame(GameMode.Host);
+            }
+            if (GUI.Button(new Rect(0,40,200,40), "Join")) {
+                StartGame(GameMode.Client);
+            }
+
+        }
+
+    }
+    
+
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,73 @@
+
+using System;
+using Fusion;
+using UnityEngine;
+
+public class Player : NetworkBehaviour
+{
+    private NetworkCharacterControllerPrototype _cc;
+    [SerializeField] private Ball _prefabBall;
+    private Vector3 _forward;
+    [Networked] private TickTimer delay {get; set;}
+    [SerializeField] private PhysxBall _prefabPhysxBall;
+
+
+
+    private void OnButton<T>(T par, Func<T> func) 
+        where T: new()
+    {
+        delay = TickTimer.CreateFromSeconds(Runner, 0.5f);
+        Runner.Spawn<T>( par,
+            transform.position + _forward,
+            Quaternion.LookRotation(_forward),
+            Object.InputAuthority
+            // (runner, o) => {
+            //     // Initialize the Ball before synchronizing
+            //     o.GetComponent<T>().Init();
+            // }
+        );
+    }
+
+    private void Awake()
+    {
+        _cc = GetComponent<NetworkCharacterControllerPrototype>();
+        _forward = transform.forward;
+    }
+
+    public override void FixedUpdateNetwork()
+    {
+        if (GetInput(out NetworkInputData data))
+        {
+            data.direction.Normalize();
+            _cc.Move(5 * Runner.DeltaTime * data.direction);
+
+            if (data.direction.sqrMagnitude > 0) {
+                _forward = data.direction;
+            }
+
+            if (delay.ExpiredOrNotRunning(Runner)) {
+                if ((data.buttons & NetworkInputData.MOUSEBUTTON1) != 0) {
+                    delay = TickTimer.CreateFromSeconds(Runner, 0.5f);
+                    Runner.Spawn(_prefabBall,
+                        transform.position + _forward, Quaternion.LookRotation(_forward),
+                        Object.InputAuthority, (runner, o) => {
+                            // Initialize the Ball before synchronizing
+                            o.GetComponent<Ball>().Init();
+                        });
+                }
+                else if ((data.buttons & NetworkInputData.MOUSEBUTTON2) != 0) {
+                    delay = TickTimer.CreateFromSeconds(Runner, 0.5f);
+                    Runner.Spawn(_prefabPhysxBall,
+                        transform.position + _forward,
+                        Quaternion.LookRotation(_forward),
+                        Object.InputAuthority,
+                        (runner, o) =>
+                        {
+                            o.GetComponent<PhysxBall>().Init(10 * _forward);
+                        });
+                }
+            }
+
+        }
+    }
+}
